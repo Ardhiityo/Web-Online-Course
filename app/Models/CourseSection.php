cd1a12e211f25344316f495d9e34367609558c69
belajar-vue-2 <aryaadi229@gmail.com>
2025-04-22 22:31:44 +0700

    feat : relations belongs to from course sections, course benefits to courses table

--- app/Models/CourseSection.php
+++ app/Models/CourseSection.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class CourseSection extends Model
 {
-    //
+    public function course()
+    {
+        return $this->belongsTo(Course::class);
+    }
 }
