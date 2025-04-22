7fa2f9ff8fe7d19f1d7fd4646357224bfd045550
belajar-vue-2 <aryaadi229@gmail.com>
2025-04-22 22:42:11 +0700

    feat : relations has many from courses to course mentors & belongs to course mentors to courses table

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -20,4 +20,9 @@ class Course extends Model
     {
         return $this->hasMany(CourseSection::class);
     }
+
+    public function courseMentors()
+    {
+        return $this->hasMany(CourseMentor::class);
+    }
 }
--- app/Models/CourseMentor.php
+++ app/Models/CourseMentor.php
@@ -10,4 +10,9 @@ class CourseMentor extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function course()
+    {
+        return $this->belongsTo(Course::class);
+    }
 }
